4c324ea70a575dd1d47b9943a81efb621690578f
Ali Tokmak <alitokmak.tr@gmail.com>
2020-01-20 18:09:21 -0800

    I added locators in login page, added test, and utilitiies classes

--- src/test/java/com/_4C/pages/Login.java
+++ src/test/java/com/_4C/pages/Login.java
@@ -1,4 +1,47 @@
 package com._4C.pages;
 
+import com._4C.utilities.ConfigurationReader;
+import com._4C.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Login {
+    public Login(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath="//a[@target='_blank'][@xpath='2']")
+    public WebElement login;
+
+    @FindBy(id ="email")
+    public WebElement username;
+
+    @FindBy(id ="password")
+    public WebElement password;
+
+    @FindBy(xpath ="//input[@value='Log In']")
+    public WebElement loginButton;
+
+    @FindBy(xpath ="//span[@class='field-error']")
+    public WebElement errorMessage;
+
+    @FindBy(xpath="test")
+    public WebElement verifyLogin;
+
+    @FindBy(xpath="test")
+    public WebElement LogoutButton;
+
+
+    public void login(String user, String pass){
+        username.sendKeys(user);
+        password.sendKeys(pass);
+        loginButton.click();
+    }
+
+    public void open(){
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+    }
+
+
 }
--- src/test/java/com/_4C/tests/Test4C.java
+++ src/test/java/com/_4C/tests/Test4C.java
@@ -1,4 +1,67 @@
 package com._4C.tests;
 
+import com._4C.pages.Login;
+import com._4C.utilities.ConfigurationReader;
+import com._4C.utilities.Driver;
+import com.sun.tools.javac.util.Assert;
+import org.apache.poi.ss.usermodel.*;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.util.concurrent.TimeUnit;
+
 public class Test4C {
+
+    Login login =new Login();
+    Actions action;
+
+    @BeforeMethod
+    public void setup(){
+        action = new Actions(Driver.getDriver());
+        Driver.getDriver().manage().timeouts().implicitlyWait(Long.valueOf(ConfigurationReader.getProperty("implicitwait")), TimeUnit.SECONDS);
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+    }
+
+    @AfterMethod
+    public void teardown(){
+        Driver.closeDriver();
+    }
+
+    //login with valid username and password
+    @Test
+    public void loginTest(){
+        login.open();
+        login.login(ConfigurationReader.getProperty("username"),ConfigurationReader.getProperty("password"));
+        Assert.check(login.verifyLogin.getText().contains("Welcome"));
+
+    }
+
+    //login with invalid username and password
+    @Test
+    public void loginNegativeTest(){
+        login.open();
+        login.login(ConfigurationReader.getProperty("username"),ConfigurationReader.getProperty("password"));
+        Assert.check(login.errorMessage.getText().contains("Specified user does not exis"));
+    }
+
+    //login with different users
+    @Test
+    public void loginWithMultibleUsers() throws Exception{
+        String path ="Customers.xlsx";
+        FileInputStream fileInputStream = new FileInputStream(path);
+        Workbook workbook = WorkbookFactory.create(fileInputStream);
+        Sheet sheet = workbook.getSheet("Sheet1");
+
+        for(int i=1; i<sheet.getLastRowNum(); i++){
+            login.open();
+            login.login(sheet.getRow(i).getCell(1).toString(),sheet.getRow(i).getCell(2).toString());
+            Assert.check(login.verifyLogin.getText().contains("Welcome"));
+            login.LogoutButton.click();
+        }
+        workbook.close();
+    }
 }
